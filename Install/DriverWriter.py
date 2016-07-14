0d30da45a40654b22524b9a060acae72bcd12f08
Williams, Michael <michael.williams3@ge.com>
2016-07-14 12:37:13 -0400

    initial commit

---
+++ Install/DriverWriter.py
@@ -0,0 +1,55 @@
+import json
+from pprint import pprint
+
+#open json file
+with open('DriverRegistryStarter.json') as data_file:
+	driverData = json.load(data_file)
+
+numDrivers = len(driverData["drivers"])
+importList = []
+nameList = []
+pinNumberList = []
+sensorObjectList = []
+dataCollectorList = []
+publisherIdList = []
+
+driverFile = open(driverData["name"], 'w')
+
+#collect json data
+for i in range(0, numDrivers):
+	importList.append(driverData["drivers"][i]["import"])
+	nameList.append(driverData["drivers"][i]["name"])
+	pinNumberList.append(driverData["drivers"][i]["pinNumber"])
+	sensorObjectList.append(driverData["drivers"][i]["sensorObject"])
+	dataCollectorList.append(driverData["drivers"][i]["dataCollector"])
+	publisherIdList.append(driverData["drivers"][i]["publisherId"])
+	
+#write imports
+driverFile.write("from time import sleep, time\nimport zmq\nfrom json import dumps\nfrom os import getenv\n")
+for i in range(0, numDrivers):
+	driverFile.write("import " + importList[i] + "\n")
+driverFile.write("\n")
+
+
+#write sensor objects
+for i in range(0, numDrivers):
+	sensorString = sensorObjectList[i].replace('pinNumber', pinNumberList[i], 1)
+	driverFile.write(nameList[i] + " = " + importList[i] + "." + sensorString + "\n")
+driverFile.write("quality = 2\n")
+
+#write zmq objects
+driverFile.write("\n#Vars from environment\nZMQ_SENSOR_PUB_IP = getenv('ZMQ_SENSOR_PUB_IP', '127.0.0.1')\nZMQ_SENSOR_PUB_PORT = getenv('ZMQ_SENSOR_PUB_PORT', 35690)\nINTERVAL_SEC = int(getenv('INTERVAL_SEC', 3))\ncontext = zmq.Context()\nsocket = context.socket(zmq.PUB)\nsocket.bind(\"tcp://%s:%s\" % (ZMQ_SENSOR_PUB_IP, ZMQ_SENSOR_PUB_PORT))\n\n")
+
+driverFile.write("while True:\n")
+
+#write data collectors
+for i in range(0, numDrivers):
+	driverFile.write("\t" + nameList[i] + "Data = " + nameList[i] + "." + dataCollectorList[i] + "\n")
+
+#write data publishers
+for i in range(0, numDrivers):
+	driverFile.write("\tsocket.send_multipart([\'" + publisherIdList[i] + "\', dumps({\"name\": \"" + nameList[i] + "\", \"datapoints\":[[int(time() * 1000), " + nameList[i] + "Data, quality]]})])\n")
+	driverFile.write("\tprint " + nameList[i] + "Data\n")
+
+driverFile.write("\n\tsleep(INTERVAL_SEC)")
+driverFile.close()
---
+++ Install/StarterKitDriverInstall.sh
@@ -0,0 +1,48 @@
+#Find upm
+cd ..
+driverpath="$PWD"
+cd upm
+
+#Install Starter Kit Drivers
+mv offered_drivers/grove src
+mv offered_drivers/rotaryencoder src
+mv offered_drivers/gas src
+mv offered_drivers/th02 src
+mv offered_drivers/guvas12d src
+mv offered_drivers/my9221 src
+mv offered_drivers/lcd src
+cd build
+echo "Compiling libraries"
+cmake .. -DBUILDSWIGNODE=OFF
+make -i
+echo "Installing libraries"
+sudo make install
+
+echo "Linking packages"
+ln -s /usr/local/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages
+
+echo "Creating drivers"
+cd .. 
+cd ..
+cd Install
+python DriverWriter.py
+mv starter_sensor_pub.py ..
+
+#Publish Service
+echo '[Unit]' > /etc/systemd/system/starter-sensor-pub.service
+echo 'Description=Grove Sensor Data Publisher' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'After=network.target' >> /etc/systemd/system/starter-sensor-pub.service
+echo '' >> /etc/systemd/system/starter-sensor-pub.service
+echo '[Service]' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'ExecStart=/usr/bin/python '$driverpath'/starter_sensor_pub.py' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'Environment=PYTHONUNBUFFERED=1' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'Restart=always' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'RestartSec=10' >> /etc/systemd/system/starter-sensor-pub.service
+echo '' >> /etc/systemd/system/starter-sensor-pub.service
+echo '[Install]' >> /etc/systemd/system/starter-sensor-pub.service
+echo 'WantedBy=multi-user.target' >> /etc/systemd/system/starter-sensor-pub.service
+
+echo "Enabling new service"
+sudo systemctl daemon-reload
+sudo systemctl start starter-sensor-pub
+sudo systemctl enable starter-sensor-pub
